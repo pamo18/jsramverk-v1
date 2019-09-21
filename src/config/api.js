f4e4bf308c7742435b84afc99493f24fb8a4755b
“Paul <pauljm80@gmail.com>
2019-09-22 01:28:53 +0200

    kmom03v2

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 const base = {
     api: function() {
         // let api = "http://localhost:8333";
-        let api = "https:me-api.pamo18.me";
+        let api = "https://me-api.pamo18.me";
         return api;
     }
 }
